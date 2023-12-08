34ff06ed3c47c717aff39a037a6408992f80cc5f
Kholekile2 <5100146@mypillar7uwc.ac.za>
2023-12-08 15:28:54 +0200

    first Commit

---
+++ Assignment3/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Assignment3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SoccerBall KickOff = new SoccerBall("White", "Sphere", "Addidas");
+
+
+            Console.WriteLine(KickOff.GetColour());
+            Console.WriteLine(KickOff.GetShape());
+            Console.WriteLine(KickOff.GetBrand());
+        }
+    }
+}
---
+++ Assignment3/SoccerBall.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3
+{
+    internal class SoccerBall
+    {
+        private string colour;
+        private string shape;
+        private string brand;
+
+        public SoccerBall(string Colour, string Shape, string Brand) 
+        {
+            colour = Colour;
+            shape = Shape;
+            brand = Brand;
+        }
+
+        public string GetColour()
+        {
+            return colour;
+        }
+        public string GetShape()
+        {
+            return shape;
+        }
+        public string GetBrand()
+        {
+            return brand;
+        }
+
+        
+
+    }
+
+}
